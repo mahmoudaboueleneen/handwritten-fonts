2038703eb0ccd42755d7a59bdf1a54e806f5ac68
Mahmoud M. Abou Eleneen <mahmoudaboueleneen@gmail.com>
2024-03-14 00:48:22 +0200

    Add loading and styling for the extension

--- Browser Extension/src/App.tsx
+++ Browser Extension/src/App.tsx
@@ -1,35 +1,15 @@
 import { useState } from "react";
-import { create } from "ipfs-http-client";
 
 const App = () => {
   const [fileUrl, updateFileUrl] = useState(``);
-
-  // const projectId = "YOUR_PROJECT_ID";
-  // const projectSecret = "YOUR_PROJECT_SECRET";
-  // const auth = `Basic ${btoa(`${projectId}:${projectSecret}`)}`;
-
-  // const client = create({ host: "ipfs.infura.io", port: 5001, protocol: "https", headers: { authorization: auth } });
-
-  // const onFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   if (!e.target.files) {
-  //     return;
-  //   }
-
-  //   const file = e.target.files[0];
-  //   try {
-  //     const added = await client.add(file);
-  //     const url = `https://ipfs.infura.io/ipfs/${added.path}`;
-  //     updateFileUrl(url);
-  //     console.log("IPFS URL: ", url);
-  //   } catch (error) {
-  //     console.log("Error uploading file: ", error);
-  //   }
-  // };
+  const [uploading, setUploading] = useState(false);
 
   const onFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) {
       return;
     }
+
+    setUploading(true);
 
     const file = e.target.files[0];
     const fileName = file.name;
@@ -57,6 +37,8 @@ const App = () => {
       }
     } catch (error) {
       console.log("Error uploading file: ", error);
+    } finally {
+      setUploading(false);
     }
   };
 
@@ -76,18 +58,40 @@ const App = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen p-5 my-2 gap-y-5 min-w-80">
       <h1 className="text-3xl font-bold">Handwritten Fonts</h1>
+      <p>Upload your generated TTF file.</p>
 
       <label className="w-full max-w-xs form-control">
         <input type="file" onChange={onFileChange} className="w-full max-w-xs file-input file-input-bordered" />
       </label>
 
-      {fileUrl && <img src={fileUrl} alt="preview" />}
+      {uploading && <span className="loading loading-dots loading-lg"></span>}
 
       <hr />
 
       <button className="btn btn-active btn-primary" onClick={onChangePageFont}>
         Change Font
       </button>
+
+      {fileUrl && (
+        <div role="alert" className="alert alert-success">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-6 h-6 stroke-current shrink-0"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <p className="break-all">
+            Your font file has been uploaded to the IPFS at <span className="font-medium">{fileUrl}</span>
+          </p>
+        </div>
+      )}
     </div>
   );
 };
